50465db92dbdeff3a6a73e54b47f582add3eef09
Chetra Chann <channchetra@gmail.com>
2023-10-27 12:31:04 +0700

    Change to maven

---
+++ src/main/java/mptc/kh/gov/demo/DmoeApplication.java
@@ -0,0 +1,13 @@
+package mptc.kh.gov.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmoeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mptc/kh/gov/demo/DmoeApplicationTests.java
@@ -0,0 +1,13 @@
+package mptc.kh.gov.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
