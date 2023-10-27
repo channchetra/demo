2ce25da3674030ca5dc4fae4c49b90bc492a9ee3
Chetra Chann <channchetra@gmail.com>
2023-10-27 12:32:24 +0700

    Add sample

--- src/main/java/mptc/kh/gov/demo/DmoeApplication.java
+++ src/main/java/mptc/kh/gov/demo/DmoeApplication.java
@@ -1,13 +1,18 @@
 package mptc.kh.gov.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class DmoeApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DmoeApplication.class, args);
-	}
-
+@RestController
+public class DemoApplication {
+    public static void main(String[] args) {
+      SpringApplication.run(DemoApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
 }
